0484f0c0d08a51726a41c2ce996dea62ccd285ea
goodboy95 <501217662@qq.com>
2018-09-05 08:46:41 +0800

    commit

--- frontend/js_src/forum/postlist.js
+++ frontend/js_src/forum/postlist.js
@@ -4,7 +4,7 @@ var Vue = require('vue');
 
 window.onload = function(){
     var queryStr = window.location.href.split('?')[1];
-    var forumId = queryStr.split('=')[1];
+    var forumId = parseInt(queryStr.split('=')[1]);
     axios.get('/api/forum/postList/', { params: {forumId: forumId, pageSize: 15, pageNum: 0} }).then(function(resp){
         if (resp.data.Code === 0) {
             var postList = resp.data.Data;
@@ -22,18 +22,15 @@ window.onload = function(){
     document.getElementById('submit').onclick = function(){
         var title = document.getElementById('title').value;
         var content = document.getElementById('content').value;
-        $.post('/api/forum/sendpost/', {
-            __RequestVerificationToken: $('#writePost').find('#token').find('input').val(),
-            forumId: forumId, 
-            title: title, 
-            content: content}, function(resp){
-            if (resp.code === 0){
-                alert('帖子发表成功！');
-                location.reload();
-            }
-            else{
-                alert(resp.msg);
-            }
+        axios.post('/api/forum/post/', {
+            title: title,
+            forumId: forumId,
+            authorId: 1,
+            content: content
+        }).then(function(resp){
+            console.log(JSON.stringify(resp));
+        }).catch(function(err){
+            console.error(err);
         });
     };
 }
